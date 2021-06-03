237c1065170a56d00f3b8f9128bc06b0243b5d77
Gaston Isnardi <gaston.isnardi@outlook.com.ar>
2021-06-03 18:17:01 -0300

    Responsive

--- webNahuel/src/app/header/brand/brand.component.ts
+++ webNahuel/src/app/header/brand/brand.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-brand',
   templateUrl: './brand.component.html',
-  styleUrls: ['./brand.component.css']
+  styleUrls: ['./brand.component.css'],
 })
 export class BrandComponent implements OnInit {
+  mobile: boolean = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    if (window.screen.width < 768) {
+      // 768px portrait
+      this.mobile = true;
+    }
   }
-
 }
